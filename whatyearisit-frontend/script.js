e87e6cdd1c0b1ca27fe45c1b0f07876b6185e7ea
Tristan <tristan.rou2@gmail.com>
2025-02-26 21:39:23 +0100

    modif pour ariane

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,12 +1,7 @@
 fetch('https://perso-rose.vercel.app/year')
 	.then(response => response.json())
 	.then(data => {
-		document.querySelector('#year').textContent = data.json;
-	});
-		
-		
-		/*innerHTML += `
-			<p>${data.year}</p>
+		document.querySelector('#year').innerHTML += `
+			<p>'${data.year}'</p>
 			`;
 		console.log(data)})
-*/
